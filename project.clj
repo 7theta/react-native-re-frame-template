ea15de9aae163e8b367f2f4e0a4cd55eecb31c72
Matthew Brown <me.matt.brown@gmail.com>
2019-07-19 17:53:19 -0400

    le bye bye -SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject react-native-re-frame/lein-template "0.1.0-SNAPSHOT"
+(defproject react-native-re-frame/lein-template "0.1.0"
   :description "A template for building React Native apps with shadow-cljs, reagent, and re-frame."
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
