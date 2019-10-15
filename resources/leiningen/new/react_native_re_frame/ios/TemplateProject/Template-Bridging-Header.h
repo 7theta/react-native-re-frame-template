53b9cd2e774d66cf6531a6ba6f562a088cdea0ea
Matthew Brown <me.matt.brown@gmail.com>
2019-10-14 20:33:45 -0400

    bridging header wip

---
+++ resources/leiningen/new/react_native_re_frame/ios/TemplateProject/Template-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <React/RCTBridge.h>
+#import <React/RCTBundleURLProvider.h>
+#import <React/RCTRootView.h>
+#import <React/RCTEventEmitter.h>
+#import <React/RCTViewManager.h>
+#import <React/RCTUIManager.h>
+#import <React/RCTLinkingManager.h>
+#import <React/RCTPushNotificationManager.h>
